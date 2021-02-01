06618ac938ee249d8b1d992f11af9da7fd18e05b
Carlos Alsina <calsina71@gmail.com>
2021-02-01 09:36:28 +0100

    primer responsive

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
-function() {
-    document.getElementsByClassName("x").onclick().style
+function hide() {
+    var x = document.getElementsByClassName("oferta");
+    x[0].style.display = "none";
 }
+
+/*function show() {
+    var x = document.getElementsByClassName("oferta");
+    x.style.display = "block";
+}*/
